386299977b77dbdab34434060c0e7cf84cf7fbf0
Roxann <rofaniry@gmail.com>
2025-08-14 16:27:25 +0300

    Initialize React project with basic App and Tailwind

--- Characters/src/App.jsx
+++ Characters/src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css'
 
 function App() {
-
   return (
-    <>
-
-    </>
-  )
+      <div className="container mx-auto p-4">
+        <h1 className="text-3xl font-bold mb-6 text-center">Character Management</h1>
+      </div>
+  );
 }
 
-export default App
+export default App;
